1d47ee5f1a1783686e818a073164931aabb366f8
RAHUL KUMAR <121605635+rajr4712@users.noreply.github.com>
2023-02-23 19:32:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let a=5;
+let b=16;
+let c=15;
+let d= a+b+c;
+document.write(d);
+console.log("hello world");
+
+//object add
+let car = {
+    model:ferrari,
+    color:black,
+    year:2022,
+    weight:40,
+
+}
+document.write(car);
+
+//function branch
+function mul(a,b) {
+    return a*b;
+}
+let obj=mul(10,30);      //function called and value passed
+document.write(obj);
+
+//array create
+let fruits = ["apple","orange","kiwi"]
+document.write(fruits);
